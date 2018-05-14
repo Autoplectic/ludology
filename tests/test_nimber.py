eebf48291a0fb9c69ef91a69c5e00b7febd8846e
Ryan James <ryangregoryjames@gmail.com>
2018-05-14 14:33:40 -0700

    more nimber tests

--- tests/test_nimber.py
+++ tests/test_nimber.py
@@ -29,3 +29,54 @@ def test_neg_nimbers():
     """
     a = Nimber(5)
     assert a == -a
+
+
+def test_fail():
+    """
+    """
+    with pytest.raises(ValueError):
+        Nimber(0.5)
+
+
+@pytest.mark.parametrize(['n', 'v'], [
+    (0, '0'),
+    (1, '∗'),
+    (6, '∗6'),
+])
+def test_nimber_value(n, v):
+    """
+    """
+    assert Nimber(n).value == v
+
+
+@pytest.mark.parametrize('n', [
+    Nimber(0),
+    Nimber(3),
+    Nimber(7),
+])
+def test_nimber_is_number(n):
+    """
+    """
+    assert not n.is_number
+
+
+@pytest.mark.parametrize('n', [
+    Nimber(0),
+    Nimber(3),
+    Nimber(7),
+])
+def test_nimber_is_impartial(n):
+    """
+    """
+    assert n.is_impartial
+
+
+@pytest.mark.parametrize('n', [
+    0,
+    3,
+    7,
+])
+def test_nimber_birthday(n):
+    """
+    """
+    assert Nimber(n).birthday == n
