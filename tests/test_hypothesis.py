49ecbc35fa5a2126888c6b4f21e749b1559e5228
Ryan James <ryangregoryjames@gmail.com>
2018-05-19 01:11:06 -0700

    expand test

--- tests/test_hypothesis.py
+++ tests/test_hypothesis.py
@@ -8,9 +8,12 @@ from ludology import Game
 from ludology.hypothesis import gamify
 
 
-def test_gamify():
+@pytest.mark.parametrize(['s', 'g'], [
+    (([([], [([], [])]), ([], [])], [([], [])]), Game({0}, {0})),
+    (Game({0}, {0}), Game({0}, {0})),
+])
+def test_gamify(s, g):
     """
     Test a contrived example.
     """
-    structure = ([([], [([], [])]), ([], [])], [([], [])])
-    assert gamify(structure) == Game({0}, {0})
+    assert gamify(s) == g
